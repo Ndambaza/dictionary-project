7b6c88a56df1835b03b5fbe37b8a05c61ca3a98d
Ndambaza <135227646+Ndambaza@users.noreply.github.com>
2025-11-12 02:16:20 +0000

    added heading

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import Dictionary from "./Dictionary";
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <header className="App-header">
+        <h1>Dictionary App</h1>
+      </header>
       <main>
         <Dictionary />
       </main>
