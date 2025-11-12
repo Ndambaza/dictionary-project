30b49b37a85a0e3aa25387e91755fb3abb49411f
Ndambaza <135227646+Ndambaza@users.noreply.github.com>
2025-11-12 14:17:56 +0000

    Phonetic component added

--- src/Meaning.js
+++ src/Meaning.js
@@ -10,10 +10,11 @@ export default function Meaning(props) {
         return (
           <div key={index}>
             <p>
+              <strong>Definition:</strong>
               {definition.definition}
               <br />
               <strong>Example:</strong>
-              <em>{definition.example}</em>
+              <em>{definition.example}</em> <strong>Synonyms:</strong>
               <Synonyms synonyms={definition.synonyms} />
             </p>
           </div>
